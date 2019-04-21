259bde3be692628d6520154f3ca193590f64c972
Jakub Stępak <jakub.ste@gmail.com>
2019-04-22 01:22:35 +0200

    Updates

---
+++ precache-manifest.325e37135790443697e5974a04915f07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb418d6561e1963c50c8",
+    "url": "/traficar/static/js/runtime~main.3c896c44.js"
+  },
+  {
+    "revision": "94c3962eb3b748f0e8d4",
+    "url": "/traficar/static/js/main.020c3042.chunk.js"
+  },
+  {
+    "revision": "a068f0a08e0d01d57e23",
+    "url": "/traficar/static/js/2.2ca30a49.chunk.js"
+  },
+  {
+    "revision": "94c3962eb3b748f0e8d4",
+    "url": "/traficar/static/css/main.93bfdb77.chunk.css"
+  },
+  {
+    "revision": "3b70c50d48ec6a9dbc95f2e438ba2020",
+    "url": "/traficar/index.html"
+  }
+];
